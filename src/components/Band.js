ae728bc66903df1ba13def4fd5e21943b6935e7a
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-03-04 17:19:50 -0800

    fixed counter

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,26 +1,30 @@
 import React, {useState} from 'react';
+import data from '../metal.json';
 import './Band.css';
 
 function Band(props) {
-    const { band_name, formed, origin, fans, split, style } = props;
+    const { band_name, formed, origin, fans, split } = props;
     const [count, setCount] = useState(0);
 return (
+    <>
   <div className='Band'>
       <h2>{band_name}</h2> 
       <h4>Origin: {origin}</h4>
       <h4>Fans: {fans} </h4>
       <h3>Formed: {formed}</h3>
-      { // if split == - 
-      split === '-' ? <div className='likes'>
-            <button classname="like-status"onClick={() => setCount(count + 1)}>
+      { split === '-' ? 
+            <div className='likes'>
+            <button onClick={() => setCount(count + 1)}>
                    <span role="img" aria-label='thumbs up'>👍</span>
             </button>
             <h3>{count}</h3>
-             <button onClick={() => setCount(count - 1)}>
+             <button onClick={() => count > 0 ? setCount(count - 1) : setCount(0)}>
                    <span role="img" aria-label='thumbs down'>👎</span>
             </button>
-      </div> : <h3>Split: {split}</h3>}
+            </div> 
+      : <h3>split: {split}</h3>}
   </div>
+  </>
   );
 }
 
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import data from '../metal.json';
+import './Heading.css';
 // count the number of bands in json data
 const bands = data.length;
-const style = data.styles.length
+const styles = data.style
 
 function Heading(props) {
    // count the number of bands
@@ -12,7 +13,7 @@ function Heading(props) {
         <h1>Merissa Bridgeman</h1>
         <h4>FEW 1.2 Final Assessment</h4>
         <h4>Total Bands: {bands}</h4>
-        <h4> Styles: {style} </h4>
+        <h4> Styles: {styles} </h4>
     </div>  
     );
 }
