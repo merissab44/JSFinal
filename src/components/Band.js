b8e650b3bf673a37cf5cc9cbe4dbdc1eca7ab7c5
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-03-04 17:28:09 -0800

    formmated number of fans

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,28 +1,18 @@
-import React, {useState} from 'react';
-import data from '../metal.json';
+import React from 'react';
 import './Band.css';
+import Like from './Like';
 
 function Band(props) {
-    const { band_name, formed, origin, fans, split } = props;
-    const [count, setCount] = useState(0);
+    const { band_name, formed, origin, fans, split, style } = props;
 return (
     <>
   <div className='Band'>
       <h2>{band_name}</h2> 
       <h4>Origin: {origin}</h4>
-      <h4>Fans: {fans} </h4>
-      <h3>Formed: {formed}</h3>
-      { split === '-' ? 
-            <div className='likes'>
-            <button onClick={() => setCount(count + 1)}>
-                   <span role="img" aria-label='thumbs up'>👍</span>
-            </button>
-            <h3>{count}</h3>
-             <button onClick={() => count > 0 ? setCount(count - 1) : setCount(0)}>
-                   <span role="img" aria-label='thumbs down'>👎</span>
-            </button>
-            </div> 
-      : <h3>split: {split}</h3>}
+      <h4>Fans: {fans.toLocaleString('en')} </h4>
+      <h4>Formed: {formed}</h4>
+      <h4>Style: {style}</h4>
+      { split === '-' ? <Like /> : <h3>split: {split}</h3>}
   </div>
   </>
   );
---
+++ src/components/Like.js
@@ -0,0 +1,19 @@
+import React, {useState } from 'react';
+
+function Like(props) {
+
+    const [count, setCount] = useState(0);
+return (
+  <div className='likes'>
+            <button onClick={() => setCount(count + 1)}>
+                   <span role="img" aria-label='thumbs up'>👍</span>
+            </button>
+            <h3>{count}</h3>
+             <button onClick={() => count > 0 ? setCount(count - 1) : setCount(0)}>
+                   <span role="img" aria-label='thumbs down'>👎</span>
+            </button>
+    </div> 
+    );
+}
+
+export default Like;
